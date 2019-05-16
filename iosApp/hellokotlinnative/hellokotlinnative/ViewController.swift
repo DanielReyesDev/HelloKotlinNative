c33fa10cefe3e4ebfefe010ec6b66ec5ed97dca7
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-15 22:47:32 -0500

    iOS Project created and configured some tasks in gradle regarding the .framework for iOS

---
+++ iosApp/hellokotlinnative/hellokotlinnative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloKotlinNative
+//
+//  Created by Daniel Reyes Sánchez on 5/15/19.
+//  Copyright © 2019 Daniel Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
