cfa0de271812da5e5c84b59328e4bc51166c3477
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-15 21:42:40 -0500

    final adjustments to android subproject before testing shared Greeting module

--- androidApp/src/main/java/com/bosch/hellokotlinnative/MainActivity.kt
+++ androidApp/src/main/java/com/bosch/hellokotlinnative/MainActivity.kt
@@ -2,11 +2,16 @@ package com.bosch.hellokotlinnative
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bosch.hellokotlinnative.Greeting
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greeting.text = Greeting().greeting()
     }
 }
